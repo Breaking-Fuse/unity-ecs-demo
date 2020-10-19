744e0541679cf0e5a68c60023408c543f2907252
BreakingFuse <BreakingFuse@hotmail.com>
2020-10-18 23:26:06 -0500

    Initial Commit
    Added Scene using ECS

---
+++ unity-ecs-demo/Assets/Scripts/Rotator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed;
+    public string direction;
+}
+
+class RotatorSystem : ComponentSystem
+{
+    struct Components
+    {
+        public Rotator rotator;
+        public Transform transform;
+    }
+
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.ForEach((Transform transform, Rotator rotator) => {
+            if (rotator.direction == "horizontal")
+            {
+                transform.Rotate(0f, rotator.speed * deltaTime, 0f);
+            }
+            else if (rotator.direction == "vertical")
+            {
+                transform.Rotate(rotator.speed * deltaTime, 0f, 0f);
+            }
+            
+        });
+    }
+}
